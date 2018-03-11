759f6e5dac753ffddbbfede9373e41c6a7c5584a
alexanderkm <maniatesa@gmail.com>
2018-03-11 19:57:33 -0400

    Change from supplier to just implements Producer. Log messages produced per second metric.

--- src/main/java/com/alex/producer/ProducerSupplier.java
+++ src/main/java/com/alex/producer/ProducerSupplier.java
@@ -1,39 +1,46 @@
 package com.alex.producer;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.Metric;
+import org.apache.kafka.common.MetricName;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.errors.ProducerFencedException;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.Supplier;
 
-public class ProducerSupplier implements Supplier<KafkaProducer<String, String>> {
+public class ProducerSupplier<K, V> implements Producer<K, V> {
 
   private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(ProducerSupplier.class);
 
   private ProducerProperties producerProperties;
   private ScheduledExecutorService executorService;
-  private AtomicReference<KafkaProducer<String, String>> mainKafkaProducer;
-  private KafkaProducer<String, String> backupKafkaProducer;
+  private AtomicReference<KafkaProducer<K, V>> mainKafkaProducer;
+  private KafkaProducer<K, V> backupKafkaProducer;
 
   public ProducerSupplier(ProducerProperties producerProperties) {
     this.producerProperties = producerProperties;
     this.executorService = Executors.newSingleThreadScheduledExecutor();
-    this.mainKafkaProducer = new AtomicReference<>(new KafkaProducer<String, String>(producerProperties.getProperties()));
+    this.mainKafkaProducer = new AtomicReference<>(new KafkaProducer<K, V>(producerProperties.getProperties()));
 
     executorService.scheduleAtFixedRate(this::rotateProducer, 35, 45, TimeUnit.SECONDS);
   }
 
-  @Override
-  public KafkaProducer<String, String> get() {
+  public KafkaProducer<K, V> get() {
     return mainKafkaProducer.get();
   }
 
   protected void rotateProducer() {
     LOG.info("Rotating producer");
-    KafkaProducer<String, String> nextProducer = new KafkaProducer<String, String>(producerProperties.getProperties());
+    KafkaProducer<K, V> nextProducer = new KafkaProducer<>(producerProperties.getProperties());
     backupKafkaProducer = mainKafkaProducer.getAndSet(nextProducer);
     try {
       Thread.sleep(2000L);
@@ -43,5 +50,65 @@ public class ProducerSupplier implements Supplier<KafkaProducer<String, String>>
     LOG.info("Closing old producer");
     backupKafkaProducer.close();
     LOG.info("Old producer is closed");
+  }
+
+  @Override
+  public void initTransactions() {
+    get().initTransactions();
+  }
+
+  @Override
+  public void beginTransaction() throws ProducerFencedException {
+    get().beginTransaction();
+  }
+
+  @Override
+  public void sendOffsetsToTransaction(Map<TopicPartition, OffsetAndMetadata> map, String s) throws ProducerFencedException {
+    get().sendOffsetsToTransaction(map, s);
+  }
+
+  @Override
+  public void commitTransaction() throws ProducerFencedException {
+    get().commitTransaction();
+  }
+
+  @Override
+  public void abortTransaction() throws ProducerFencedException {
+    get().abortTransaction();
+  }
+
+  @Override
+  public Future<RecordMetadata> send(ProducerRecord<K, V> producerRecord) {
+    return get().send(producerRecord);
+  }
+
+  @Override
+  public Future<RecordMetadata> send(ProducerRecord<K, V> producerRecord, Callback callback) {
+    return get().send(producerRecord, callback);
+  }
+
+  @Override
+  public void flush() {
+    get().flush();
+  }
+
+  @Override
+  public List<PartitionInfo> partitionsFor(String s) {
+    return get().partitionsFor(s);
+  }
+
+  @Override
+  public Map<MetricName, ? extends Metric> metrics() {
+    return get().metrics();
+  }
+
+  @Override
+  public void close() {
+    get().close();
+  }
+
+  @Override
+  public void close(long l, TimeUnit timeUnit) {
+    get().close(l, timeUnit);
   }
 }
--- src/main/java/com/alex/producer/WrappedKafkaProducer.java
+++ src/main/java/com/alex/producer/WrappedKafkaProducer.java
@@ -2,19 +2,17 @@ package com.alex.producer;
 
 import com.alex.core.ImmutableTimedMessage;
 import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Snapshot;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.Metric;
-import org.apache.kafka.common.MetricName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
@@ -30,14 +28,16 @@ public class WrappedKafkaProducer {
   private final ObjectMapper objectMapper;
   private final ScheduledExecutorService executorService;
   private final Histogram produceTimeHistogram;
-  private final ProducerSupplier producerSupplier;
+  private final Meter messagesMeter;
+  private final Producer<String, String> delegate;
 
   public WrappedKafkaProducer() {
     this.properties = new ProducerProperties();
     this.objectMapper = new ObjectMapper();
     this.executorService = Executors.newSingleThreadScheduledExecutor();
     this.produceTimeHistogram = new MetricRegistry().histogram("produce-time");
-    this.producerSupplier = new ProducerSupplier(properties);
+    this.messagesMeter = new MetricRegistry().meter("messages-produced");
+    this.delegate = new ProducerSupplier<>(properties);
   }
 
   public String getTopic() {
@@ -80,13 +80,14 @@ public class WrappedKafkaProducer {
             .text("Some message text")
             .build();
         String messageText = objectMapper.writeValueAsString(message);
-        futures.add(producerSupplier.get().send(new ProducerRecord<>(getTopic(), messageText)));
+        futures.add(delegate.send(new ProducerRecord<>(getTopic(), messageText)));
       }
       futures.forEach(f -> {
         try {
           long startTime = System.currentTimeMillis();
           f.get(MESSAGE_TIMEOUT, TimeUnit.MILLISECONDS);
           produceTimeHistogram.update(System.currentTimeMillis() - startTime);
+          messagesMeter.mark();
         } catch (Exception e) {
           LOG.error("Failed to produce message", e);
         }
@@ -99,8 +100,10 @@ public class WrappedKafkaProducer {
 
   protected void reportMetrics() {
     Snapshot snapshot = produceTimeHistogram.getSnapshot();
+
+    /*
     StringBuilder sb = new StringBuilder();
-    Map<MetricName, ? extends Metric> metrics = producerSupplier.get().metrics();
+    Map<MetricName, ? extends Metric> metrics = delegate.metrics();
     for (MetricName metricName : metrics.keySet()) {
       sb.append("\t");
       sb.append(metricName.name());
@@ -109,11 +112,13 @@ public class WrappedKafkaProducer {
       sb.append("\n");
     }
     LOG.info("All metrics:\n {}", sb);
+    */
     LOG.info("Local produce time 75th: {}", snapshot.get75thPercentile());
     LOG.info("Local produce time 99th: {}", snapshot.get99thPercentile());
+    LOG.info("Messages produced per second: {}", messagesMeter.getOneMinuteRate());
   }
 
   public void close() {
-    producerSupplier.get().close(5000, TimeUnit.MILLISECONDS);
+    delegate.close(5000, TimeUnit.MILLISECONDS);
   }
 }
